d53cd33c127985826f649a466c3d403cb9116461
Dom231 <114959233+Dom231@users.noreply.github.com>
2023-02-28 17:06:22 -0600

    routes(basic)

---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const reactionRoutes = require('./reactionRoutes');
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes')
+
+
+router.use('/reactions', reactionRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts',thoughtRoutes)
+
+module.exports = router;
---
+++ routes/api/reactionRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {
+  
+ 
+  createReaction,
+
+  deleteReaction,
+} = require('../../controllers/courseController.js');
+
+// /api/userss
+router.route('/').post(createReaction);
+
+// /api/users/:userId
+router
+  .route('/:userId')
+  .delete(deleteReaction);
+
+module.exports = router;
